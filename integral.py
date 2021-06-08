4d69161757a76a1db9ac613b8ab94761bf8edf7f
AZysk0 <arko.zystem@gmail.com>
2021-06-09 00:35:49 +0300

    changes

--- integral.py
+++ integral.py
@@ -12,12 +12,13 @@ def left_rectangle(a, b) -> float:
     dx = (b - a) / n # step of argument x
 
     for i in range(n):
+        # print(f'{i}\t{a + i * dx}\t{function(a + i * dx)}')
         area += function(a + i * dx)
 
     area *= dx
     print(area)
 
-    return area
+    return round(area, 5)
 
 
 def right_rectangle(a, b) -> float:
@@ -26,12 +27,13 @@ def right_rectangle(a, b) -> float:
     dx = (b - a) / n
 
     for i in range(1, n + 1):
+        # print(f'{i}\t{a + i * dx}\t{function(a + i * dx)}')
         area += function(a + i * dx)
 
     area *= dx
     print(area)
 
-    return area
+    return round(area, 5)
 
 
 def middle_rectange(a, b) -> float:
@@ -40,21 +42,29 @@ def middle_rectange(a, b) -> float:
     dx = (b - a) / n
 
     for i in range(n):
+        # print(f'{i}\t{a + i * dx + dx / 2}\t{function(a + i * dx + dx / 2)}')
         area += function(a + i * dx + dx / 2)
 
     area *= dx
     print(area)
 
-    return area
+    return round(area, 5)
 
 
 def trapezoid(a, b) -> float:
     '''trapezoid method'''
     n = 10000
+    area = 0
     dx = (b - a) / n
-    
 
-    pass
+    for i in range(n):
+        # print(f'{i}\t{a + i * dx}\t{function(a + i * dx)}')
+        area += (function(a + i * dx)
+               + function(a + (i + 1) * dx)) / 2
+
+    area *= dx
+
+    return round(area, 5)
 
 
 def simpson(a, b) -> float:
@@ -63,5 +73,5 @@ def simpson(a, b) -> float:
 
 
 left_rectangle(1, 2)
-right_rectangle(1, 2)
-middle_rectange(1, 2)
+# right_rectangle(1, 2)
+# middle_rectange(1, 2)
