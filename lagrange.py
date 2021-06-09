2e237c4114ade822af49f8c3313832120cf43c21
AZysk0 <arko.zystem@gmail.com>
2021-06-09 13:41:49 +0300

    changes

--- lagrange.py
+++ lagrange.py
@@ -10,8 +10,18 @@ y = np.array([-20, -5, 6, 25])
 
 poly = lagrange(x, y)
 coefs = Polynomial(poly).coef
+print(coefs)
 
-func = lambda x: coefs[0] * x ** 3 + coefs[1] * x ** 2  + coefs[2] * x + coefs[3]
+
+# func = lambda x: coefs[0] * x ** 3 + coefs[1] * x ** 2  + coefs[2] * x + coefs[3]
+def func(x):
+    res = 0
+    power = len(coefs) - 1
+    for coef in coefs:
+        res += coef * x ** power
+        power -= 1
+
+    return res
 
 # answers #########
 x_ans = np.array([-0.5, 0.5, 1.5, 2.5]) # answer arguments
